695042c8e3c3c5672a88b735671fbcc140945130
沫筱希 <i@nightmulti.com>
2023-01-07 12:32:15 +0800

    Initial commit

---
+++ src/main/kotlin/com/lowiro/arcaea/ArcaeaApplication.kt
@@ -0,0 +1,11 @@
+package com.lowiro.arcaea
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArcaeaApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArcaeaApplication>(*args)
+}
---
+++ src/test/kotlin/com/lowiro/arcaea/ArcaeaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lowiro.arcaea
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArcaeaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
