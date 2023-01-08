d168d20a3aea0c0ef3b21ac7f828523a44e14f4a
沫筱希 <i@nightmulti.com>
2023-01-08 20:14:07 +0800

    Update Api Pack RestController

--- src/main/kotlin/com/lowiro/arcaea/api/pack/PackRestController.kt
+++ src/main/kotlin/com/lowiro/arcaea/api/pack/PackRestController.kt
@@ -9,9 +9,29 @@ import org.springframework.web.bind.annotation.*
 class PackRestController constructor(var packMapper: PackMapper) {
 
     @PostMapping(value = ["/info"])
-    fun info(@RequestBody requestBody: JSONObject): String {
-        val id: Int = requestBody.getInteger("id") ?: return "'id' is 'null'"
-        return "'id' is '$id'"
+    fun info(@RequestBody requestBody: JSONObject): Any {
+        var id: Int = requestBody.getInteger("id") ?: return onJsonReturn("204", "\"information\": \"required: 'id'\"")
+
+        val pack: JSONObject = packMapper.packSelect(id)
+
+        if (pack.isEmpty()) {
+            return onJsonReturn("204", "\"information\": \"select failure\"")
+        }
+
+        id = pack.getInteger("id")
+
+        val platform: JSONObject = pack.getJSONObject("platform")
+        val cover: JSONObject = pack.getJSONObject("cover")
+        val title: String = pack.getString("title")
+        val type: JSONObject = pack.getJSONObject("type")
+        val count: JSONObject = pack.getJSONObject("count")
+        val date: JSONObject = pack.getJSONObject("date")
+        val cost: JSONObject = pack.getJSONObject("cost")
+
+        val upsilon =
+            "\"pack\": {\"id\": $id,\"platform\": $platform,\"cover\": $cover,\"title\": \"$title\",\"type\": $type,\"count\": $count,\"date\": $date,\"cost\": $cost}"
+
+        return onJsonReturn("200", upsilon)
     }
 
     @PostMapping(value = ["/admin/insert"])
@@ -47,12 +67,7 @@ class PackRestController constructor(var packMapper: PackMapper) {
         return if (packInsert == 0) {
             onJsonReturn("204", "\"information\": \"insert failure\"")
         } else {
-            val upsilon =
-                "\"pack\": {\"id\": $id,\"platform\": $platform,\"cover\": $cover,\"title\": \"$title\",\"type\": $type,\"count\": $count,\"date\": $date,\"cost\": $cost}"
-
-            onJsonReturn("200", upsilon)
-
-            //return onJsonReturn("200", "\"information\": \"insert success\"")
+            return onJsonReturn("200", "\"information\": \"insert success\"")
         }
     }
 
