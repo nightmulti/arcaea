f6961afa9cf2086f17bdc0047eb6670ca3d368f1
沫筱希 <i@nightmulti.com>
2023-01-08 09:45:12 +0800

    Update App Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.0")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.1")
+    implementation("com.alibaba:fastjson:2.0.22")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
