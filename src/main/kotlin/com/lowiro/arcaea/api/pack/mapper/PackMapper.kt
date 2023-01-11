dc0ee2169c268928b5ac52faacf79ed03c450298
沫筱希 <i@nightmulti.com>
2023-01-11 23:26:32 +0800

    Update PackMapper

--- src/main/kotlin/com/lowiro/arcaea/api/pack/mapper/PackMapper.kt
+++ src/main/kotlin/com/lowiro/arcaea/api/pack/mapper/PackMapper.kt
@@ -1,24 +1,17 @@
 package com.lowiro.arcaea.api.pack.mapper
 
-import com.alibaba.fastjson.JSONObject
 import org.apache.ibatis.annotations.*
 
 @Mapper
 interface PackMapper {
 
-    @Insert("INSERT INTO `pack` (`id`, `platform`, `cover`, `title`, `type`, `count`, `date`, `cost`) VALUES (#{id}, #{platform}, #{cover}, #{title}, #{type}, #{count}, #{date}, #{cost})")
-    fun packInsert(
-        @Param(value = "id") id: Int,
-        @Param(value = "platform") platform: String,
-        @Param(value = "cover") cover: String,
-        @Param(value = "title") title: String,
-        @Param(value = "type") type: String,
-        @Param(value = "count") count: String,
-        @Param(value = "date") date: String,
-        @Param(value = "cost") cost: String
-    ): Int
+    @Insert("INSERT INTO `new` (`id`, `data`) VALUES (#{id}, #{data})")
+    fun packInsert(@Param(value = "id") id: Int, @Param(value = "data") data: String): Int
 
-    @Select("SELECT * FROM `pack` WHERE id = #{id}")
-    fun packSelect(@Param(value = "id") id: Int): JSONObject
+    @Select("SELECT `id` FROM `new` ORDER BY `id` desc LIMIT 1")
+    fun packIdSelect(): Int
+
+    @Select("SELECT `data` FROM `new` WHERE `id` = #{id}")
+    fun packDataSelect(@Param(value = "id") id: Int): String
 
 }
