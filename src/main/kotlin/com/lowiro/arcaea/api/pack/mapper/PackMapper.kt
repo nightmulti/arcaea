8edd2fd7c98df23e3c5a0f762f52d117813c006e
沫筱希 <i@nightmulti.com>
2023-01-16 16:54:05 +0800

    Update PackMapper: Fix #1 table name

--- src/main/kotlin/com/lowiro/arcaea/api/pack/mapper/PackMapper.kt
+++ src/main/kotlin/com/lowiro/arcaea/api/pack/mapper/PackMapper.kt
@@ -5,13 +5,13 @@ import org.apache.ibatis.annotations.*
 @Mapper
 interface PackMapper {
 
-    @Insert("INSERT INTO `new` (`id`, `data`) VALUES (#{id}, #{data})")
+    @Insert("INSERT INTO `pack` (`id`, `data`) VALUES (#{id}, #{data})")
     fun packInsert(@Param(value = "id") id: Int, @Param(value = "data") data: String): Int
 
-    @Select("SELECT `id` FROM `new` ORDER BY `id` desc LIMIT 1")
+    @Select("SELECT `id` FROM `pack` ORDER BY `id` desc LIMIT 1")
     fun packIdSelect(): Int
 
-    @Select("SELECT `data` FROM `new` WHERE `id` = #{id}")
+    @Select("SELECT `data` FROM `pack` WHERE `id` = #{id}")
     fun packDataSelect(@Param(value = "id") id: Int): String
 
 }
