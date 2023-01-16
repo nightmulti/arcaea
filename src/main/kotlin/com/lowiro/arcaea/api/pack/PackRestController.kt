245254978e799b2da7c5148002f3a6b4584fdd51
沫筱希 <i@nightmulti.com>
2023-01-16 17:22:17 +0800

    Update PackMapper: Fix #1 return data type

--- src/main/kotlin/com/lowiro/arcaea/api/pack/PackRestController.kt
+++ src/main/kotlin/com/lowiro/arcaea/api/pack/PackRestController.kt
@@ -1,5 +1,6 @@
 package com.lowiro.arcaea.api.pack
 
+import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.JSONObject
 import com.lowiro.arcaea.api.pack.mapper.PackMapper
 import org.springframework.web.bind.annotation.*
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.*
 class PackRestController constructor(val packMapper: PackMapper) {
 
     @PostMapping(value = ["/info"])
-    fun info(@RequestBody requestBody: JSONObject): String {
+    fun info(@RequestBody requestBody: JSONObject): JSONObject {
         val packDataSelect = packMapper.packDataSelect(requestBody.getInteger("id") ?: return omicron("required: 'id'"))
         return try {
             content(packDataSelect)
@@ -19,25 +20,18 @@ class PackRestController constructor(val packMapper: PackMapper) {
     }
 
     @PostMapping(value = ["/admin/insert"])
-    fun insert(@RequestBody requestBody: JSONObject): String {
+    fun insert(@RequestBody requestBody: JSONObject): JSONObject {
         val data = requestBody.getJSONArray("data")
-        var message = "required: '平台'"
         try {
             data[0]
-            message = "required: '曲包封面'"
             data[1]
-            message = "required: '曲包名称'"
             data[2]
-            message = "required: '曲包类型'"
             data[3]
-            message = "required: '内含曲目数量'"
             data[4]
-            message = "required: '上线时间'"
             data[5]
-            message = "required: '花费'"
             data[6]
         } catch (exception: Exception) {
-            return omicron(message)
+            return omicron("required: 'data'")
         }
         return if (packMapper.packInsert(packMapper.packIdSelect() + 1, data.toString()) == 0) {
             omicron("insert failure")
@@ -49,15 +43,15 @@ class PackRestController constructor(val packMapper: PackMapper) {
     val server = "\"server\": {\"version\": {\"omicron\": \"15\"}}"
     val copyright = "\"copyright\": \"2023\""
 
-    fun result(sigma: String, upsilon: String): String {
-        return "{\"contents\": {\"answers\": {\"results\": {\"sigma\": \"$sigma\",\"upsilon\": $upsilon},$server},$copyright}}"
+    fun result(sigma: String, upsilon: String): JSONObject {
+        return JSON.parseObject("{\"contents\": {\"answers\": {\"results\": {\"sigma\": \"$sigma\",\"upsilon\": $upsilon},$server},$copyright}}")
     }
 
-    fun omicron(omicron: String): String {
+    fun omicron(omicron: String): JSONObject {
         return result("204", "\"$omicron\"")
     }
 
-    fun content(answer: String): String {
+    fun content(answer: String): JSONObject {
         return result("200", answer)
     }
 
